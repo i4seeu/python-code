4d399bcdb9fb546c06487a9849528538eeeb6aeb
Noordeen Malango <noordinmalango@gmail.com>
2019-09-24 10:01:16 +0200

    is legible

--- classess/cla.py
+++ classess/cla.py
@@ -11,6 +11,11 @@ class Student:
     self.age = age
   def getAge(self):
     return self.age
+  def isLegible(self):
+    if self.age >= 35:
+      return True
+    else:
+      return False
 student1 = Student("Noordeen Malango","RG123",35)
 student1.assessPassMark(40)
 student1.changeName("Vanessa Narciso")
@@ -18,3 +23,6 @@ student1.assessPassMark(40)
 student1.setAge(60)
 student_age = student1.getAge()
 print(student_age)
+print(student1.isLegible())
+student1.setAge(20)
+print(student1.isLegible())
