7835fe3430ee5af8801ec7d21dff201f3d025415
Noordeen Malango <noordinmalango@gmail.com>
2019-09-24 08:53:38 +0200

    student object

---
+++ classess/cla.py
@@ -0,0 +1,18 @@
+class Student:
+  def __init__(self , name, id,age):
+    self.name = name
+    self.id = id
+    self.age = age
+
+student1 = Student("Noordeen Malango","RG123",35)
+student2 = Student("Rina7 Narciso", "MA222",0)
+
+
+print(student1.name)
+print(student1.id)
+print(student1.age)
+
+print(student2.name)
+print(student2.id)
+print(student2.age)
+
