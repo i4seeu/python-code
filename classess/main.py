4dc03d45070e9845d6c370d327e9d05a62517ca2
VN-7 <vnarciso@softhacksai.com>
2019-09-25 15:42:33 +0200

    modules

--- classess/main.py
+++ classess/main.py
@@ -1,10 +1,5 @@
-import names
-import school as xul
-from cars import Audi
-names.printName("Noordeen Malango")
+import countries
 
-com = xul.School("College of Medicine","Lilongwe","Malawi","1944")
-print(com.getName())
+co = countries.Country("Malawi")
+print(co.getName())
 
-r8 = Audi("Beam 2000",4,"1994","black")
-print(r8.getEngine())
